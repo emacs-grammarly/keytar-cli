6e4df390456aa40b26ec774f9d5d8fa22193880d
Jen-Chieh <jcs090218@gmail.com>
2021-04-28 16:10:52 +0800

    Add test script

---
+++ test/commands.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# ========================================================================
+# $File: commands.sh $
+# $Date: 2021-04-28 16:04:12 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2021 by Shen, Jen-Chieh $
+# ========================================================================
+
+cd ..
+
+npm install
+
+CMD="node index.js"
+
+echo "'keytar-cli' version: "
+$CMD --version
